8eb568b563c3e18ca3a87629e08853b63ba2c184
=TheNameless0ne <=alexfinsher@gmail.com>
2021-03-11 22:45:37 +0300

    Выполнил Task 01. Простейшее автоматное программирование на С++

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "fun.h"
+#include <iostream>
 
-int main()
-{
+using namespace std;
+
+int main() {
+    cout << " 1. String: fwr r4 4 f da3de2 \n    Result: " << faStr1("fwr r4 4 f da3de2 ") << endl;
+    cout << " 2. String: Arr weSw 5asd A#da Asff2d AdsfGa\n    Result: " << faStr2("Arr weSw 5asd A#da Asff2d AdsfGa") << endl;
+    cout << " 3. String: Arr weSw 5asd A#da Asff2d AdsfGa\n    Result: " << faStr3("Arr weSw 5asd A#da Asff2d AdsfGa") << endl;
     return 0;
 }
